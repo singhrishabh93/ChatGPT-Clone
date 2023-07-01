304f6835fc1cd18533a947ca56e85b6ae12a01ed
singhrishabh93 <singhrishabh1670@gmal.com>
2023-07-01 10:58:13 +0530

    MongoDB Added

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
 from flask import Flask, render_template, jsonify, request
+from flask_pymongo import PyMongo
 
+app = Flask(__name__)
+app.config["MONGO_URI"] = "mongodb+srv://singhrishabh1670:Re%40lmeBook@gptclone.31q8ngu.mongodb.net/chatgpt"
+mongo = PyMongo(app)
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template("index.html")
+    chats = mongo.db.chats.find({})
+    myChats = [chat for chat in chats]
+    print(myChats)
+    return render_template("index.html", myChats = myChats)
 
 @app.route("/api", methods=["GET", "POST"])
 def qa():
