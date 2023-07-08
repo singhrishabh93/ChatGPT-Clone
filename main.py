51f1c8e289e4531130595aefa7c47ee65a0d7eb1
singhrishabh93 <singhrishabh1670@gmal.com>
2023-07-08 21:00:35 +0530

    Integrated OpenAI API

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 from flask import Flask, render_template, jsonify, request
 from flask_pymongo import PyMongo
 
+import openai
+
+openai.api_key = "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"
+
+
+
 app = Flask(__name__)
 app.config["MONGO_URI"] = "mongodb+srv://singhrishabh1670:Re%40lmeBook@gptclone.31q8ngu.mongodb.net/chatgpt"
 mongo = PyMongo(app)
@@ -25,7 +31,22 @@ def qa():
             return jsonify(data)
         else:
             data = {"result": f"Answer of {question}"}
-            mongo.db.chats.insert_one({"question" : question, "answer" : f"Answer from open AI for {question}"})
+            response = openai.ChatCompletion.create(
+                    model="gpt-3.5-turbo",
+                    prompt=question,
+                    messages=[
+                        {
+                        "role": "user",
+                        "content": ""
+                        }
+                    ],
+                    temperature=1,
+                    max_tokens=256,
+                    top_p=1,
+                    frequency_penalty=0,
+                    presence_penalty=0
+                    )
+            mongo.db.chats.insert_one({"question" : question, "answer" : response})
             return jsonify(data)
     data = {"result": "Thanks"}
     return jsonify(data)
